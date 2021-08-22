732cfd2314be89f5d33970b4bb3d622ae4d8fc74
Ashly Draper <ashly_draper@msn.com>
2021-08-22 15:46:49 -0500

    revisions from review

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,18 +10,18 @@ export default function Main(props) {
             <section className="profile">
                 <div className="profile__info-box">
                     <div className="profile__avatar-wrapper">
-                        <img id="avatar" src={currentUser.avatar} alt="avatar" className={`profile__avatar ${props.isLoading}`} />
+                        <img id="avatar" src={currentUser.avatar} alt="avatar" className="profile__avatar"/>
                         <div onClick={props.onEditAvatarClick} className="profile__avatar-overlay"></div>
                     </div>
                     <div className="profile__text">
                         <div className="profile__text-top">
-                            <h1 className={`profile__name $${props.isLoading}`}>{currentUser.name}</h1>
-                            <button onClick={props.onEditProfileClick} type="button" name="Edit profile" className={`profile__edit-button ${props.isLoading}`} aria-label="Edit profile"></button>
+                            <h1 className= "profile__name" >{currentUser.name}</h1>
+                            <button onClick={props.onEditProfileClick} type="button" name="Edit profile" className= "profile__edit-button"  aria-label="Edit profile"></button>
                         </div>
                         <p className={`profile__title ${props.isLoading}`}>{currentUser.about}</p>
                     </div>
                 </div>
-                <button onClick={props.onAddPlaceClick} type="button" name="Add image" className={`profile__add-button ${props.isLoading}`} aria-label="Add image"></button>
+                <button onClick={props.onAddPlaceClick} type="button" name="Add image" className="profile__add-button" aria-label="Add image"></button>
             </section>
             <section className="cards">
                 {props.cards.map((card, index) => (
